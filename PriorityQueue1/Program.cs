927ff2ad9205538ab954e1350b5e002d36998893
SirMalelo <davidech1994@outlook.com>
2025-09-11 14:04:41 -0500

    Update PriorityQueue1/Program.cs: worked on priority queue implementation and logic

--- PriorityQueue1/Program.cs
+++ PriorityQueue1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PriorityQueue<string, int> queue = new PriorityQueue<string, int>();
+        queue.Enqueue("Java", 50);
+        queue.Enqueue("Python", 45);
+        queue.Enqueue("JavaScript", 70);
+        queue.Enqueue("C#", 1);
+ 
+        while (queue.TryDequeue(out string item, out int priority))
+        {
+            Console.WriteLine($"The priority of the programming language {item} is {priority} ");
+        }
     }
 }
+ 
